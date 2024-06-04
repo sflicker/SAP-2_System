96b7a94861501bd2cc0093bde91e8a1b8d6bcc94
Scott Flicker <sflicker2@earthlink.net>
2024-06-04 00:45:17 -0400

    updating assembly test files

--- asm_test_files/test_jmp_expect_12_v2.asm
+++
@@ -1,7 +0,0 @@
-0000: 3E 11              -- MVI A, 11H      
-0002: C3 08 00           -- JMP 0008        
-0005: D3 03              -- OUT 3           ;do not expected this output of 11H
-0007: 76                 -- HLT             
-0008: 3C                 -- INR A           
-0009: D3 03              -- OUT 3           ;expect output 12H
-000B: 76                 -- HLT             
